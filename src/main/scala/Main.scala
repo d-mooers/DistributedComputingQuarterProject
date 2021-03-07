0140ab9c36817a16b90b35086f690b3973165e48
Justin Feinfield <jfeinfield@gmail.com>
2021-03-06 16:35:46 -0800

    start cleaning, upload test data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+package example
 import org.apache.spark.SparkContext._
 import scala.io._
 import org.apache.spark.{ SparkConf, SparkContext }
@@ -5,17 +6,53 @@ import org.apache.spark.rdd._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import scala.collection._
+import org.apache.spark
 
 object Main {
-  val INPUT_FILE = "input/..."
+  val INPUT_FILE = "data/test.csv"
 
   def main(args: Array[String]): Unit = {
     System.setProperty("hadoop.home.dir", "c:/winutils/")
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
 
-    val conf = new SparkConf().setAppName("labsix").setMaster("local[4]")
+    val conf = new SparkConf().setAppName("asgn").setMaster("local[4]")
     val sc = new SparkContext(conf)
-    val lineItems = sc.textFile(INPUT_FILE).map(_.split(", ").toList);
+    
+    val lineItems = sc.textFile(INPUT_FILE).flatMap(_.split("\n")).map(_.split(","));
+
+    val cleaned = lineItems.map(x => x.slice(5,8) ++ x.slice(9, 15) :+ x(18))
+    cleaned.take(3).foreach(x => {
+      x.foreach(println(_))
+    })
+
+  }
+
+  val manufMap = immutable.Map("ford" -> 0,
+                              "chevrolet" -> 1,
+                              "toyota" -> 2,
+                              "jeep" -> 3,
+                              "nissan" -> 4,
+                              "honda" -> 5,
+                              "ram" -> 6,
+                              "gmc" -> 7,
+                              "dodge" -> 8,
+                              "hyundai" -> 9)
+
+  val condMap = immutable.Map("new" -> 0,
+                                "like new" -> 1,
+                                "excellent" -> 2,
+                                "good" -> 3,
+                                "fair" -> 4,
+                                "salvage" -> 5)
+
+  val cylindMap = immutable.Map("4 cylinders" -> 0,
+                                "6 cylinders" -> 1,
+                                "8 cylinders" -> 2)
+
+
+  // price, year, manuf, condition, cylinders, fuel, odom, title, trans, type
+  def clean(entry: Array[String]) : Array[String] = {
+
   }
 }
