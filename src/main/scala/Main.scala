8c371dad2e805fdb7303bd702104e5f304f82d70
Justin Feinfield <jfeinfield@gmail.com>
2021-03-07 11:29:31 -0800

    add more cleaning code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -21,11 +21,9 @@ object Main {
     
     val lineItems = sc.textFile(INPUT_FILE).flatMap(_.split("\n")).map(_.split(","));
 
-    val cleaned = lineItems.map(x => x.slice(5,8) ++ x.slice(9, 15) :+ x(18))
-    cleaned.take(3).foreach(x => {
-      x.foreach(println(_))
-    })
+    val cleaned = lineItems.map(x => x.slice(5,8) ++ x.slice(9, 15)).map(entry => clean(entry)).filter(_.length == 9)
 
+    println(cleaned.count())
   }
 
   val manufMap = immutable.Map("ford" -> 0,
@@ -50,9 +48,65 @@ object Main {
                                 "6 cylinders" -> 1,
                                 "8 cylinders" -> 2)
 
+  val fuelMap = immutable.Map("gas" -> 0,
+                              "diesel" -> 1)
 
-  // price, year, manuf, condition, cylinders, fuel, odom, title, trans, type
-  def clean(entry: Array[String]) : Array[String] = {
+  val titleMap = immutable.Map("clean" -> 0,
+                              "rebuilt" -> 1,
+                              "salvage" -> 2)
+
+  val transMap = immutable.Map("automatic" -> 0,
+                                "manual" -> 1)
+
+  // price, year, manuf, condition, cylinders, fuel, odom, title, trans
+  def clean(entry: Array[String]) : List[Double] =  {
+    var retArray = mutable.MutableList[Double]()
+    for (i <- 0 to 8) {
+      if (i == 0 || i == 1 || i == 6) {
+        val a = try {
+          entry(i).toDouble
+        } catch {
+          case e : Exception => ""
+        }
+        if (a == "")
+          return List(0.0)
+        
+        retArray = retArray :+ a.toString.toDouble
+      }
+      else
+        retArray = retArray :+ 0.0
+    }
+    if (manufMap.contains(entry(2)))
+      retArray(2) = manufMap(entry(2))
+    else
+      return List(0.0)
+
+    if (condMap.contains(entry(3)))
+      retArray(3) = condMap(entry(3))
+    else
+      return List(0.0)
+
+    if (cylindMap.contains(entry(4)))
+      retArray(4) = cylindMap(entry(4))
+    else
+      return List(0.0)
+
+    if (fuelMap.contains(entry(5)))
+      retArray(5) = fuelMap(entry(5))
+    else
+      return List(0.0)
+
+    if (titleMap.contains(entry(7)))
+      retArray(7) = titleMap(entry(7))
+    else
+      return List(0.0)
+
+    if (transMap.contains(entry(8)))
+      retArray(8) = transMap(entry(8))
+    else
+      return List(0.0)
+
+    return retArray.toList
 
   }
 }
